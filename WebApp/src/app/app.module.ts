58e32b4fec2cf80c289e4253e35ab83db5c24e8b
unknown <felipecarvalhosamuel@gmail.com>
2019-03-30 17:50:34 -0300

    Imports utilizados adicionados

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,10 @@ import { CidadeComponent } from './cadastros/cidade/cidade.component';
 import { PagenotfoundComponent } from './erros/pagenotfound/pagenotfound.component';
 import { FreteComponent } from './cadastros/frete/frete.component';
 import { TabelafreteComponent } from './cadastros/tabelafrete/tabelafrete.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule, MatRadioModule, MatButtonModule, MatTableModule, MatIconModule,
+   MatPaginatorModule, MatSort, MatSortModule, MatExpansionModule, MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -20,9 +26,23 @@ import { TabelafreteComponent } from './cadastros/tabelafrete/tabelafrete.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatRadioModule,
+    MatButtonModule, 
+    MatTableModule, 
+    MatIconModule, 
+    HttpClientModule,
+    MatPaginatorModule, 
+    MatSortModule, 
+    MatExpansionModule, 
+    MatSelectModule
+
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
