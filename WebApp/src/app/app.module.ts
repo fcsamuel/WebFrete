5554ba41921e952ef528c24c53773d703805bbb7
MatheusSantosScuzziatto <matheusscuzzi@gmail.com>
2019-03-29 21:45:51 -0300

    Pacotes models e cadastros adicionados

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EstadoComponent } from './Model/estado/estado.component';
+import { CidadeComponent } from './Model/cidade/cidade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstadoComponent,
+    CidadeComponent
   ],
   imports: [
     BrowserModule,
