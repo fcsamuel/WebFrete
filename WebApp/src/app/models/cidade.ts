51994e3ae708936b3f3966d6829f438d3cd57731
MatheusSantosScuzziatto <matheusscuzzi@gmail.com>
2019-03-29 22:09:08 -0300

    cidade.ts arrumada

--- WebApp/src/app/models/cidade.ts
+++ WebApp/src/app/models/cidade.ts
@@ -1,9 +1,8 @@
 import { Estado } from "./estado";
-import { Cep } from "./cep";
 
 export class Cidade {
     public cidadeId: number;
     public descricao: string;
     public estado: Estado;
-    public cepList: Array<Cep>;
+    public cepList: Array<string>;
 }
